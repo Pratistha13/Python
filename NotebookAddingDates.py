35cbc229b9d3c310d9c30c7354e76cf202c933ad
Pratistha Mishra <56651666+Pratistha13@users.noreply.github.com>
2020-04-04 12:52:27 +0300

    Update NotebookAddingDates.py

--- NotebookAddingDates.py
+++ NotebookAddingDates.py
@@ -1,4 +1,9 @@
 import time
+
+'''In this exercise, add a feature which includes the date and time of the written note to the program.
+The program works as earlier, but saves data in the form "[note]:::[date and time]" meaning that there 
+is a three-colon separator between the note and timestamp.'''
+
 print("(1) Read the notebook \n(2) Add note \n(3) Empty the notebook \n(4) Quit")
 
 myfile = "notebook.txt"
